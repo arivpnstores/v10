fe2335da4ca80a26a9ec6503c7da568691118635
ARI VPN STORES <arivpnstores@gmail.com>
2025-09-01 03:40:33 +0700

    v10

--- Fls/limit.sh
+++ Fls/limit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-REPOSLIM="https://raw.githubusercontent.com/arivpnstores/v10/main/"
-wget -q -O /usr/bin/limit-ip "${REPOSLIM}Fls/limit-ip"
+REPOSLIM="https://raw.githubusercontent.com/Diah082/vip/main/"
+wget -q -O /usr/bin/limit-ip "${REPOSLIM}install/limit-ip"
 chmod +x /usr/bin/*
 cd /usr/bin
 sed -i 's/\r//' limit-ip
 cd
 systemctl daemon-reload
-wget -q -O /etc/systemd/system/limitvmess.service "${REPOSLIM}Fls/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPOSLIM}Fls/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPOSLIM}Fls/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPOSLIM}Fls/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPOSLIM}Fls/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPOSLIM}Fls/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPOSLIM}Fls/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPOSLIM}Fls/shadowsocks" >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvmess.service "${REPOSLIM}install/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPOSLIM}install/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPOSLIM}install/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPOSLIM}install/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vmess "${REPOSLIM}install/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPOSLIM}install/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPOSLIM}install/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPOSLIM}install/shadowsocks" >/dev/null 2>&1
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
